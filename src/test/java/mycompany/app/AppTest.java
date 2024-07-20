7570dbed473ecb3f9ae981f17ca7236660c945be
szehui <114173250+tszehui@users.noreply.github.com>
2024-07-21 00:13:59 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://host.docker.internal";
+	String url = "192.168.10.172:8080";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
